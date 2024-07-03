4ae8cd53516d721d9a1642e00e74b766a028cf7b
Florian Marchive <marchiveflorian@gmail.com>
2024-07-03 11:31:22 +0200

    first commit

---
+++ hashmap/src/main/java/fr/afpa/App.java
@@ -0,0 +1,80 @@
+package fr.afpa;
+
+//TODO Faites l'import de java HashMap
+import java.util.HashMap;
+
+public class App {
+    public static void main(String[] args) {
+
+        // Ressources :
+        // https://www.youtube.com/watch?v=I9aBP0xm-lE
+        // https://www.w3schools.com/java/java_hashmap.asp
+        // https://www.jmdoudoux.fr/java/dej/chap-collections.htm#collections-5
+
+        // TODO Instanciez une nouvelle HashMap sur le theme de votre choix
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        // TODO Ajoutez des paires clé/valeur à votre HashMap - au moins 5 paires
+        capitalCities.put("Angleterre", "Londres");
+        capitalCities.put("France", "Paris");
+        capitalCities.put("Allemagne", "Berlin");
+        capitalCities.put("Espagne", "Madrid");
+        capitalCities.put("Italie", "Rome");
+
+        // TODO Récupérez la valeur d'une de vos paires
+        System.out.println("La valeur récupérée est: " + capitalCities.get("Allemagne"));
+
+        // TODO Vérifiez si une de vos clés est contenue dans la collection
+        System.out.println("La clé en paramètre est présente: " + capitalCities.containsKey("Italie"));
+
+        // TODO Vérifiez si une de vos valeurs est contenue dans la collection
+        System.out.println("La valeur en paramètre est présente: " + capitalCities.containsValue("Londres"));
+
+        // TODO Ajoutez une paire clé/valeur à votre HashMap
+        capitalCities.put("Belgique", "Bruxelles");
+
+        // TODO Utilisez une méthode pour trouver le nombre de paires (la longueur de
+        // votre HashMap) et affichez la en console
+        System.out.println("Nombre de paires clé/valeur: " + capitalCities.size());
+
+        // TODO Affichez toutes les clés de votre HashMap (avec une boucle for each &
+        // avec une méthode de HashMap)
+        System.out.println("Voici les clés de notre HashMap: ");
+        for (String i : capitalCities.keySet()) {
+            System.out.println(i);
+        }
+
+        System.out.println(capitalCities.keySet());
+
+        // TODO Affichez toutes les valeurs de votre HashMap (avec une boucle for each &
+        // avec une méthode de HashMap)
+        System.out.println("Voici les valeurs de notre HashMap: ");
+        for (String i : capitalCities.values()) {
+            System.out.println(i);
+        }
+
+        System.out.println(capitalCities.values());
+
+        // TODO Affichez toutes les clés/valeurs de votre HashMap (avec une boucle for
+        // each & avec une méthode de HashMap)
+        System.out.println("Voici les paires de notre HashMap: ");
+        for (String i : capitalCities.keySet()) {
+            System.out.println("key: " + i + " value: " + capitalCities.get(i));
+        }
+
+        System.out.println(capitalCities.entrySet());
+
+        // TODO Supprimez une de vos paires
+        capitalCities.remove("Espagne");
+
+        // TODO Vérifiez si votre HashMap est vide
+        System.out.println("La Hashmap est vide? :" + capitalCities.isEmpty());
+
+        // TODO Supprimez tous les éléments de votre HashMap
+        capitalCities.clear();
+
+        // TODO Refaites la vérification
+        System.out.println("La Hashmap est vide? :" + capitalCities.isEmpty());
+
+    }
+}
